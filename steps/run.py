81e245b05d52864d9a309997c197c13d8f1bfe85
Jakob S Kottmann <jakob.kottmann@gmail.com>
2021-03-03 20:47:34 -0500

    update

--- steps/run.py
+++ steps/run.py
@@ -3,6 +3,8 @@ import numpy, json
 from isingdata import CircuitGenerator, simplified_ising, CircuitGenEncoder
 
 def run_ising_circuits(n_qubits, g=1.0, *args, **kwargs):
+    import multiprocessing as mp
+    print(mp.cpu_count())
     H = simplified_ising(n_qubits=n_qubits, g=g)
     if n_qubits < 10:
         exact_gs = numpy.linalg.eigvalsh(H.to_matrix())[0]
